45986b9c71b76ae3e8e2b19b4c116de41dd88bb2
ccx19322 <chenchaoxu19322@163.com>
2017-02-17 09:38:08 +0800

    Initial Commit

---
+++ TabBarCenterButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarCenterButton
+//
+//  Created by chen cx on 2017/2/17.
+//  Copyright © 2017年 chen cx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TabBarCenterButton/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TabBarCenterButton
+//
+//  Created by chen cx on 2017/2/17.
+//  Copyright © 2017年 chen cx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
